b79319d69c19be26d2148caf0bbbbcecbd9d34aa
Ivan <ivaneremov@icloud.com>
2019-11-29 10:20:42 +0300

    Добавьте файлы проекта.

---
+++ your_Blog/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="your_Blog.MvcApplication" Language="C#" %>
---
+++ your_Blog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(your_Blog.Startup))]
+namespace your_Blog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
